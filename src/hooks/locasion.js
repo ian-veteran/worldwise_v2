d0194a089f757a2d762d51add9c57f5fbe0ee125
ian-veteran <iankaranimutuma@gmail.com>
2024-07-18 12:21:29 +0300

    innitial_commit

---
+++ src/hooks/locasion.js
@@ -0,0 +1,21 @@
+function getPosition() {
+    if (!navigator.geolocation)
+      return setError("Your browser does not support geolocation");
+
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+  }
+
+  return { isLoading, position, error, getPosition };
